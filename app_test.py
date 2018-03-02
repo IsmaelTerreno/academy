5ee5ff5a9097972299a3c8986370a97a9e15a247
Ismael Terreno <iterreno@zigatta.com>
2018-03-02 16:16:05 -0300

    More basic testing.

--- app_test.py
+++ app_test.py
@@ -1,11 +1,19 @@
 import unittest
+import app_api
 
 
 class AppTestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.app = app_api.app.test_client()
+
     def test_config_db(self):
-        print('first test')
         self.assertEqual(True, True)
+
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        print(rv.data)
+        assert b'Welcome!' in rv.data
 
 
 if __name__ == '__main__':
