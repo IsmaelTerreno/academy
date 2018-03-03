1f4251eb537f20898efb361a6277c54179046d84
Ismael Terreno <iterreno@zigatta.com>
2018-03-02 21:56:48 -0300

    Some refactor name

--- app_test.py
+++ app_test.py
@@ -12,7 +12,19 @@ class AppTestCase(unittest.TestCase):
         rv = self.app.get('/')
         assert b'Welcome!' in rv.data
 
-    def test_create_a_teacher(self):
+    def test_should_create_a_teacher(self):
+        response = self.app.post('/teacher', data=json.dumps(dict(
+            name='Dario Test',
+            last_name='Hilton test'
+        )), content_type='application/json', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        json_response = json.loads(response.get_data(as_text=True))
+        message = str(json_response['message'])
+        id_techer = str(json_response['id'])
+        self.assertTrue(message.__eq__('New student created!'))
+        self.assertFalse(id_techer.__eq__(''))
+
+    def test_should_view_teacher_by_id(self):
         response = self.app.post('/teacher', data=json.dumps(dict(
             name='Dario Test',
             last_name='Hilton test'
