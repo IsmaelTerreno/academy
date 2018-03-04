232c9ec65c65a1f03ba1a59d8ce6b08bc2cc4fb4
Ismael Terreno <iterreno@zigatta.com>
2018-03-04 17:41:19 -0300

    Student and Teacher not found.

--- app_test.py
+++ app_test.py
@@ -1,7 +1,6 @@
 import unittest
 import app_api
 import json
-import time
 
 
 class AppTestCase(unittest.TestCase):
@@ -42,6 +41,15 @@ class AppTestCase(unittest.TestCase):
         print('Should find teacher by id.')
         print('')
 
+    def test_should_view_teacher_by_id_not_found(self):
+        id_teacher = 'no-existing-teacher'
+        response_view_by_id = self.app.get('/teacher/{}'.format(id_teacher), follow_redirects=True)
+        json_response_view = json.loads(response_view_by_id.get_data(as_text=True))
+        message = str(json_response_view['message'])
+        self.assertTrue(message.__eq__('No teacher found!'))
+        print('Should not find teacher by id.')
+        print('')
+
     def test_should_create_a_student(self):
         response = self.app.post('/student', data=json.dumps(dict(
             name='Marcelo Test',
@@ -69,6 +77,15 @@ class AppTestCase(unittest.TestCase):
         id_teacher_view = str(json_response_view['id'])
         self.assertTrue(id_teacher_view.__eq__(id_student))
         print('Should find student by id.')
+        print('')
+
+    def test_should_view_student_by_id_not_found(self):
+        id_student = 'no-existing-student'
+        response_view_by_id = self.app.get('/student/{}'.format(id_student), follow_redirects=True)
+        json_response_view = json.loads(response_view_by_id.get_data(as_text=True))
+        message = str(json_response_view['message'])
+        self.assertTrue(message.__eq__('No student found!'))
+        print('Should not find student by id.')
         print('')
 
 
